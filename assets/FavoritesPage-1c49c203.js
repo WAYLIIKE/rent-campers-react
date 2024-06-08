7d8dcc3ef5cb62b3fcd5526ee5422b918a5ebcd7
WAYLIIKE <116974702+WAYLIIKE@users.noreply.github.com>
2024-06-08 09:00:17 +0000

    Deploying to gh-pages from @ WAYLIIKE/rent-campers-react@d87dba0e821a0ec85fef54ad78a39a4a1a531ef1 🚀

---
+++ assets/FavoritesPage-1c49c203.js
@@ -0,0 +1,1 @@
+import{j as e,H as r}from"./index-5551795f.js";import{C as s}from"./Container-39ab9051.js";function n(){return e.jsxs("div",{children:[e.jsx(r,{children:e.jsx("title",{children:"Favorites - Campervaneo"})}),e.jsx(s,{children:e.jsx("p",{children:"Blue cheese"})})]})}export{n as default};
--- assets/HomePage-8e0aacc2.js
+++
@@ -1,1 +0,0 @@
-import{j as e,H as r}from"./index-9d62ddbf.js";function i(){return e.jsx("div",{children:e.jsx(r,{children:e.jsx("title",{children:"Home - Campervaneo"})})})}export{i as default};
